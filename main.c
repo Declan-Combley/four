818e96bd701cc1d20030cc31b802a89314f53a56
declan <declan-combley@protonmail.com>
2022-08-17 22:33:27 +0800

    update

--- main.c
+++ main.c
@@ -17,6 +17,11 @@ char *alloc(Free *f, const char *value)
     return s;
 }
 
+size_t get_end(size_t sz)
+{
+    return sz % 10;
+}
+
 size_t get_tens(size_t sz)
 {
     size_t first = sz;
@@ -36,6 +41,7 @@ char *get_string_equivelent(size_t sz, Free *f)
     }
     
     const char ones[9][7] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    const char teens[9][9] = {"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
     const char tens[9][8] = {"ten", "twenty", "thirty", "fourty", "fifty", "sixty", "sevennty", "eighty", "ninety"};
 
     if (sz < 9) {
@@ -48,7 +54,25 @@ char *get_string_equivelent(size_t sz, Free *f)
         return alloc(f, tens[first - 1]);
     }
 
+    if (sz > 10 && sz < 20) {
+        return alloc(f, teens[sz - 11]);
+    }
+
+    char *first = alloc(f, tens[get_tens(sz) - 1]);
+    char *second = alloc(f, ones[get_end(sz) - 1]);
+
+    strcat(first, " ");
+    strcat(first, second);
+    printf("%s\n", first);
     return NULL;
+
+//    char *s = malloc(sizeof(sizeof(tens[get_tens(sz) - 1]) + sizeof(ones[get_end(sz) - 1])));
+//
+//    strcpy(s, first);
+//    f->malloced[f->index] = s;
+//    f->index++;
+//
+//    return s;
 }
 
 void free_malloced(Free *f)
@@ -62,10 +86,10 @@ int main(void)
 {
     Free to_be_freed = {0};
     
-    char *str = "threethree";
-    size_t len = strlen(str);
+    char *str = "threethreet";
+    //size_t len = strlen(str);
 
-    str = get_string_equivelent(len, &to_be_freed);
+    str = get_string_equivelent(23, &to_be_freed);
     printf("output: %s\n", str);
 
     free_malloced(&to_be_freed);
